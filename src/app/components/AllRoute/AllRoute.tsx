6f81594317522abec271c6340d47814a1ad1b2d7
Shajadulhaqueemon111 <mdshajdulhaqueemon@gmail.com>
2025-08-23 23:07:24 +0600

    added a navbar

---
+++ src/app/components/AllRoute/AllRoute.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../Navbar";
+
+const AllRoute = () => {
+  return (
+    <div>
+      <div className="mx-auto items-center">
+        <Navbar />
+      </div>
+    </div>
+  );
+};
+
+export default AllRoute;
